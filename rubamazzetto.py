e0a70efd8bf22edf41b9b328628566dbd4f6bb64
74C17N3P7UN3 <leocalzavara05@gmail.com>
2022-08-28 17:33:20 +0200

    Initial commit

---
+++ rubamazzetto.py
@@ -0,0 +1,31 @@
+# ------------------------------------------- CardGames.py/rubamazzetto.py ------------------------------------------- #
+
+__author__ = "TacitNeptune"
+__github__ = "https://github.com/74C17N3P7UN3/CardGames.py"
+
+__version__ = "a0.0.1"
+__updated__ = "28/08/2022"
+
+# ------------------------------------------------- Python Packages -------------------------------------------------- #
+import sys
+
+
+# ---------------------------------------------------- Main Class ---------------------------------------------------- #
+class Rubamazzetto:
+    def __init__(self):
+        pass
+
+    def handle_args(self):
+        pass
+
+    def arg_help(self):
+        pass
+
+    def main(self):
+        pass
+
+
+# ------------------------------------------------ Main Entry Point -------------------------------------------------- #
+if __name__ == '__main__':
+    Game = Rubamazzetto()  # OS Object
+    Game.main()  # Run Program
