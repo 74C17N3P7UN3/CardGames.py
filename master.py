269142bdcfaadbd661bc6427a0f5c45b999146e9
74C17N3P7UN3 <leocalzavara05@gmail.com>
2022-08-30 11:53:43 +0200

    Refactored master.py

--- master.py
+++ master.py
@@ -12,6 +12,8 @@ class Master:
         self.current = 0
         self.total = len(self.games) - 1
 
+        self.game_space = ' ' * round((80 - 31) / 2)
+
         if len(self.args) > 0:
             self.handle_args()
 
@@ -19,28 +21,28 @@ class Master:
     def handle_args(self):
         # --------------------------------------------- Master Arguments --------------------------------------------- #
         if self.args[0] == "--help" or self.args[0] == "--h":
-            print(f"{Colors.green}█ This is the launcher for the various games")
+            print(f"{Colors.green}█ Help for the master launcher")
             print(f"\n█ Args available:")
-            print(f"{Colors.lb}█  --help: shows this prompt")
+            print(f"{Colors.lc}█  --help: shows this prompt")
+            print(f"{Colors.lc}█  --version: shows the launcher's version")
             print(f"{Colors.lb}█  --list: shows the games list usable as argument")
-            print(f"{Colors.lb}█  --version: shows the launcher's version")
-            print(f"{Colors.green}█ Run without args to use the GUI and the default settings")
-            print(f"\n{Colors.green}█ To play a game with modified settings, use the following syntax:")
-            print(f"{Colors.red}█  python .\\master.py --[gameName] --[gameArgs]")
-            print(f"\n{Colors.yellow}█ To get the games list, use '--list' instead of --[gameName]")
-            print(f"{Colors.yellow}█ To get game-specific args, use '--help' after --[gameName]")
+            print(f"\n{Colors.green}█ Run without args to use the GUI and the default games settings")
+            print(f"{Colors.green}█ To play a game with modified settings, use the following syntax:")
+            print(f"{Colors.red}█ python .\\master.py [--gameName] [--gameArgs]")
+            print(f"\n{Colors.yellow}█ To get the games list, use '--list' instead of [--gameName]")
+            print(f"{Colors.yellow}█ To get game-specific args, use '--help' after [--gameName]")
             print(f"\n{Colors.red}█ Example usage: .\\master.py --rubamazzetto --verbose")
+            exit()
+        elif self.args[0] == "--version" or self.args[0] == "--v":
+            print(f"{Colors.green}█ Version: {__version__}")
+            print(f"{Colors.green}█ Updated: {__updated__}")
             exit()
         elif self.args[0] == "--list":
             print(f"{Colors.green}█ This is the games list usable as argument:")
             for game in self.games:
                 print(f"{Colors.lb}█  --{game}")
-            print(f"\n{Colors.red}█ Usage: python .\\master.py --[gameName] --[gameArgs]")
-            print(f"{Colors.yellow}█ To get game-specific args, use '--help' after --[gameName]")
-            exit()
-        elif self.args[0] == "--version" or self.args[0] == "--v":
-            print(f"{Colors.green}█ Version: {__version__}")
-            print(f"{Colors.green}█ Updated: {__updated__}")
+            print(f"\n{Colors.red}█ Usage: python .\\master.py [--gameName] [--gameArgs]")
+            print(f"{Colors.yellow}█ To get game-specific args, use '--help' after [--gameName]")
             exit()
 
         # ---------------------------------------------- Game Arguments ---------------------------------------------- #
@@ -59,14 +61,13 @@ class Master:
             while not kb.is_pressed("enter"):
                 self.select_game()
             input()  # To catch the exit key 'enter'
-
             self.launch_game(self.current)
         except KeyboardInterrupt:
             print(f"\n{Colors.red}Exit the hard way, I guess.{Colors.reset}")
-
-        cursor.show()
+            cursor.show()
 
     def launch_game(self, i: int):
+        cursor.show()
         if self.games[i] == "rubamazzetto":
             Rubamazzetto().main()
 
@@ -93,15 +94,15 @@ class Master:
     def update_selection(self):
         clear()
         print(master_banner)
-        print(f"{' ' * round((80 - 31) / 2)}╔═════════════════════════════╗")
-        print(f"{' ' * round((80 - 31) / 2)}║        SELECT A GAME        ║")
+        print(f"{self.game_space}╔═════════════════════════════╗")
+        print(f"{self.game_space}║        SELECT A GAME        ║")
         for i in range(len(self.games)):
             game = self.games[i].capitalize()
             if i == self.current:
-                print(f"{' ' * round((80 - 31) / 2)}║ > {game}{' ' * (26 - len(game))}║")
+                print(f"{self.game_space}║ > {game}{' ' * (26 - len(game))}║")
             else:
-                print(f"{' ' * round((80 - 31) / 2)}║   {game}{' ' * (26 - len(game))}║")
-        print(f"{' ' * round((80 - 31) / 2)}╚═════════════════════════════╝")
+                print(f"{self.game_space}║   {game}{' ' * (26 - len(game))}║")
+        print(f"{self.game_space}╚═════════════════════════════╝")
 
 
 # ------------------------------------------------ Main Entry Point -------------------------------------------------- #
